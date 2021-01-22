7a1821861b8c79eb8ee6ad598885261eb3afeecd
Deltanboi <pedroogheneochuko@gmail.com>
2021-01-22 01:55:47 +0100

    Add footer component

--- components/common/MainLayout.js
+++ components/common/MainLayout.js
@@ -1,10 +1,12 @@
-import Navbar from "./Navbar";
+import Footer from './Footer';
+import Navbar from './Navbar';
 
 function MainLayout({ children }) {
   return (
-    <div className="w-screen h-screen overflow-x-hidden">
+    <div className="w-screen h-screen overflow-x-hidden text-black">
       <Navbar />
       <div className="w-full flex-grow h-full pt-16">{children}</div>
+      <Footer />
     </div>
   );
 }
