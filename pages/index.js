9e65ad259eb8bcc7ec6e6a7eb95955cf6a41b67e
Deltanboi <pedroogheneochuko@gmail.com>
2021-01-25 02:27:27 +0100

    Formatting

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import { Avatar, Input, Select } from "antd";
-import Head from "next/head";
-import Link from "next/link";
-import { MainLayout } from "../components/common";
-import makeApiCall from "../utils/makeApiCall";
+import { Avatar, Input, Select } from 'antd';
+import Head from 'next/head';
+import Link from 'next/link';
+import { MainLayout } from '../components/common';
+import makeApiCall from '../utils/makeApiCall';
 
 function Home({ tags, states }) {
   const { Option } = Select;
+
   return (
     <MainLayout>
       <Head>
@@ -21,7 +22,7 @@ function Home({ tags, states }) {
         <p className="font-semibold text-2xl mb-4">Find the right job for you!</p>
         <Input.Group compact className="w-full max-w-xl">
           <Select
-            style={{ width: "40%" }}
+            style={{ width: '40%' }}
             size="large"
             defaultActiveFirstOption={true}
             defaultValue="jobs"
@@ -30,7 +31,7 @@ function Home({ tags, states }) {
             <Option value="company">Company</Option>
           </Select>
           <Input.Search
-            style={{ width: "60%" }}
+            style={{ width: '60%' }}
             size="large"
             placeholder="Search..."
             // enterButton="Search"
@@ -43,19 +44,19 @@ function Home({ tags, states }) {
         </p>
         <div
           className="w-full justify-around flex text-lg flex-wrap"
-          style={{ maxWidth: "1000px" }}
+          style={{ maxWidth: '1000px' }}
         >
           <p className="shadow rounded max-w-md p-8 text-center mt-4">
             <Link href="/signup?selected=company">
               <a className="text-green-700 font-semibold">SIGN UP</a>
-            </Link>{" "}
+            </Link>{' '}
             as an Employer and register your company/business and gain access to potential
             employees.
           </p>
           <p className="shadow rounded max-w-md p-8 text-center mt-4">
             <Link href="/signup?selected=seeker">
               <a className="text-green-700 font-semibold">SIGN UP</a>
-            </Link>{" "}
+            </Link>{' '}
             as a job seeker, build your profile, upload your CV and start applying for your dream
             job!
           </p>
@@ -66,7 +67,7 @@ function Home({ tags, states }) {
         <hr className="w-full max-w-xs mb-8" />
         <div
           className="w-full grid grid-cols-4 tablet:grid-cols-3 smallTablet:grid-cols-2 phone:grid-cols-1"
-          style={{ maxWidth: "1500px" }}
+          style={{ maxWidth: '1500px' }}
         >
           {[1, 2, 3, 4, 5, 6, 7, 8].map((job, index) => (
             <Link href="#" key={index}>
@@ -93,7 +94,7 @@ function Home({ tags, states }) {
         <p className="text-2xl mb-2 font-semibold">Job Categories</p>
         <hr className="w-full max-w-xs mb-8" />
         {tags ? (
-          <ul className="ul-list-spread" style={{ maxWidth: "1500px" }}>
+          <ul className="ul-list-spread" style={{ maxWidth: '1500px' }}>
             {tags.map((tag, index) => (
               <Link href={`/search?tag=${tag.name}`} key={index}>
                 <a className="hover:text-green-700 hover:underline">
@@ -116,7 +117,7 @@ export async function getStaticProps(context) {
   try {
     // let res = await makeApiCall("/states/get-states", { method: "get" });
     // states = res.data.states;
-    let { data } = await makeApiCall({ url: "/tags/get-tags", method: "get" });
+    let { data } = await makeApiCall({ url: '/tags/get-tags', method: 'get' });
     // let res = await axios.get("/tags/get-tags");
     tags = data.tags;
   } catch (error) {
