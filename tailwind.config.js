16cb503e9a460f6bb3377b85e20d542bee7d1fad
Deltanboi <pedroogheneochuko@gmail.com>
2021-01-25 12:51:52 +0100

    Add primary color to tailwindcs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,26 @@
 module.exports = {
-  purge: ["./components/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  purge: ['./components/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       screens: {
-        tablet: { max: "800px" },
+        tablet: { max: '800px' },
         // => @media (max-width: 800px) { ... }
 
-        smallTablet: { max: "600px" },
+        smallTablet: { max: '600px' },
         // => @media (max-width: 600px) { ... }
 
-        phone: { max: "450px" },
+        phone: { max: '450px' },
         // => @media (max-width: 350px) { ... }
+      },
+      colors: {
+        primary: '#4A916B',
       },
     },
   },
   variants: {
     // ...
-    borderWidth: ["responsive"],
-    borderWidth: ["responsive", "hover", "focus"],
-    margin: ["responsive", "hover", "focus"],
+    borderWidth: ['responsive'],
+    borderWidth: ['responsive', 'hover', 'focus'],
+    margin: ['responsive', 'hover', 'focus'],
   },
 };
