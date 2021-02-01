7298febbbb88c4b359bf3afcb1cebf3809033c8b
Deltanboi <pedroogheneochuko@gmail.com>
2021-02-01 02:54:21 +0100

    Wrap app with Global Context

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
+import GlobalContext from '../store/globalContext';
+import useGlobalState from '../store/useGlobalState';
 import '../styles/globals.css';
 
 // axios.defaults.baseURL = 'https://hire-me-o.herokuapp.com/api';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const store = useGlobalState();
+  return (
+    <GlobalContext.Provider value={store}>
+      <Component {...pageProps} />
+    </GlobalContext.Provider>
+  );
 }
 
 export default MyApp;
