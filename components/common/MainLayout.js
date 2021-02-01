a133f9c9172bf1ad226da31f943454ee9cbecee0
Deltanboi <pedroogheneochuko@gmail.com>
2021-02-01 02:55:26 +0100

    Add user login check to MainLayout
    Check is performed everytime app loads

--- components/common/MainLayout.js
+++ components/common/MainLayout.js
@@ -1,13 +1,38 @@
+import { Spinner } from 'evergreen-ui';
+import { useContext, useEffect, useState } from 'react';
+import GlobalContext from '../../store/globalContext';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
 function MainLayout({ children }) {
+  const [loading, setLoading] = useState(true);
+  const { state, actions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    setLoading(true);
+    const user = JSON.parse(localStorage.getItem('user'));
+    const token = localStorage.getItem('userToken');
+
+    if (user && token) actions({ type: 'login', payload: { user, token } });
+    else actions({ type: 'setIsLoggedIn' });
+
+    setLoading(false);
+  }, []);
+
   return (
-    <div className="w-full h-screen flex flex-col text-black">
-      <Navbar />
-      <div className="w-full flex-grow pt-16">{children}</div>
-      <Footer />
-    </div>
+    <>
+      {loading ? (
+        <div className="w-full h-screen flex justify-center items-center">
+          <Spinner size={50} />
+        </div>
+      ) : (
+        <div className="w-full h-screen flex flex-col text-black">
+          <Navbar />
+          <div className="w-full flex-grow pt-16">{children}</div>
+          <Footer />
+        </div>
+      )}
+    </>
   );
 }
 
