838130d90d5cd827ca2649656455e24831eada2e
Deltanboi <pedroogheneochuko@gmail.com>
2021-02-10 03:07:10 +0100

    Tryiing to fix deploy error

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,20 @@ module.exports = {
       test: /\.less$/,
       use: [
         {
-          loader: "style-loader",
+          loader: 'style-loader',
         },
         {
-          loader: "css-loader", // translates CSS into CommonJS
+          loader: 'css-loader', // translates CSS into CommonJS
         },
         {
-          loader: "less-loader", // compiles Less to CSS
+          loader: 'less-loader', // compiles Less to CSS
           options: {
             lessOptions: {
               // If you are using less-loader@5 please spread the lessOptions to options directly
               modifyVars: {
-                "primary-color": "#2dad67",
-                "link-color": "#2dad67",
-                "border-radius-base": "2px",
+                'primary-color': '#2dad67',
+                'link-color': '#2dad67',
+                'border-radius-base': '2px',
               },
               javascriptEnabled: true,
             },
@@ -26,6 +26,14 @@ module.exports = {
         },
       ],
       // ...other rules
+    },
+    {
+      test: /\.m?jsx?$/,
+      resolve: {
+        fullySpecified: false,
+      },
+      // ignore transpiling JavaScript from node_modules as they should be ready to go OOTB
+      // exclude: /node_modules/,
     },
   ],
   // ...other config
